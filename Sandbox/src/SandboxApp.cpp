111b837ba0cdde7710db9872b23909fb85eb8a67
TerraCrafterE3 <terracraftere3@gmail.com>
2023-07-28 09:18:15 +0200

    Better SandboxApp.cpp

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -5,16 +5,19 @@ class Sandbox : public Titan::Application
 public:
 	Sandbox()
 	{
-
+		TITAN_INFO ("This is 'TITAN_INFO()'");
+		TITAN_WARN ("This is 'TITAN_WARN()'");
+		TITAN_ERROR("This is 'TITAN_ERROR()'");
+		TITAN_TRACE("This is 'TITAN_TRACE()'");
+		TITAN_FATAL("This is 'TITAN_FATAL()'");
 	}
 
 	~Sandbox() 
 	{
-
+		TITAN_WARN("Here it destroyes the application :)");
 	}
 };
 
 Titan::Application* Titan::CreateApplication() {
-
 	return new Sandbox();
 }
--- TitanEngine/src/Titan/Log.h
+++ TitanEngine/src/Titan/Log.h
@@ -26,11 +26,11 @@ namespace Titan {
 #define TITAN_CORE_INFO(...)     ::Titan::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define TITAN_CORE_WARN(...)     ::Titan::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define TITAN_CORE_ERROR(...)    ::Titan::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define TITAN_CORE_FATAL(...)    ::Titan::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define TITAN_CORE_FATAL(...)    ::Titan::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 // Client log macros
-#define TITAN_TRACE(...)	      ::Titan::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define TITAN_INFO(...)	          ::Titan::Log::GetClientLogger()->info(__VA_ARGS__)
-#define TITAN_WARN(...)	          ::Titan::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define TITAN_ERROR(...)	      ::Titan::Log::GetClientLogger()->error(__VA_ARGS__)
-#define TITAN_FATAL(...)	      ::Titan::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define TITAN_TRACE(...)	     ::Titan::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define TITAN_INFO(...)	         ::Titan::Log::GetClientLogger()->info(__VA_ARGS__)
+#define TITAN_WARN(...)	         ::Titan::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define TITAN_ERROR(...)	     ::Titan::Log::GetClientLogger()->error(__VA_ARGS__)
+#define TITAN_FATAL(...)	     ::Titan::Log::GetClientLogger()->critical(__VA_ARGS__)
