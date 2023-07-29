6309dec479fbe698a171199761da5719a5151f7b
TerraCrafterE3 <terracraftere3@gmail.com>
2023-07-29 16:46:11 +0200

    Precompiled header files

---
+++ TitanEngine/tipch.cpp
@@ -0,0 +1,1 @@
+#include "tipch.h"
---
+++ TitanEngine/tipch.h
@@ -0,0 +1,20 @@
+#pragma once
+
+//includes
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+//platform specific includes
+#ifdef TI_PLATFORM_WINDOWS
+	#include <Windows.h>
+#endif
+
