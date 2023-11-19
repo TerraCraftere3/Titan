ea9cc4c844703d4d563a6bf33303ca913f2da8c0
TerraCrafterE3 <terracraftere3@gmail.com>
2023-11-19 12:29:47 +0100

    First triangle

--- TitanEngine/src/Platform/OpenGL/OpenGLContext.cpp
+++ TitanEngine/src/Platform/OpenGL/OpenGLContext.cpp
@@ -18,6 +18,11 @@ namespace Titan {
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		TI_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		TITAN_CORE_INFO("OpenGL Info:");
+		TITAN_CORE_INFO("  Vendor: {0}", (const char*)glGetString(GL_VENDOR));
+		TITAN_CORE_INFO("  Renderer: {0}", (const char*)glGetString(GL_RENDERER));
+		TITAN_CORE_INFO("  Version: {0}", (const char*)glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
